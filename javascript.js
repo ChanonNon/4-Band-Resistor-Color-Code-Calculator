f84fd5e8198da22f964ec97e8344fec061c98864
ChanonNon <nonchak42@gmail.com>
2022-08-10 16:08:53 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,43 @@
+var color_2 = document.getElementById("color_2");
+var black = document.getElementById("black");
+
+color_2.onclick = function(){
+    menuToggle_1()
+}
+
 function menuToggle() {
+    document.getElementById('h3').innerHTML = "กรุณาเลือกสีแถวที่ 1";
+    const toggleMenu = document.querySelector('.menu');
+    toggleMenu.classList.toggle('active')
+    
+}
+
+function menuToggle_1() {
+    document.getElementById('h3').innerHTML = "กรุณาเลือกสีแถวที่ 2";
+    const toggleMenu = document.querySelector('.menu');
+    toggleMenu.classList.toggle('active');
+}
+
+function menuToggle_2() {
+    document.getElementById('h3').innerHTML = "กรุณาเลือกสีแถวที่ 3";
     const toggleMenu = document.querySelector('.menu');
     toggleMenu.classList.toggle('active')
 }
 
 function color(color){
-    document.getElementById("color").style.background = color;
+    document.getElementById("color_1").style.background = color;
     document.getElementById("line_1").style.background = color;
 }
+
+
+
+function color(color){
+    document.getElementById("color_2").style.background = color;
+    document.getElementById("line_2").style.background = color;
+}
+
+// function color_2(color){
+//     document.getElementById("color_3").style.background = color;
+//     document.getElementById("line_3").style.background = color;
+// }
+
